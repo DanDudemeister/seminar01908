787817c56cae8950df6d713eb6d087c169598ef1
Dan <DanDudemeister@users.noreply.github.com>
2017-10-02 18:15:32 +0200

    added angular-material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppComponent } from './app.component';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {MdButtonModule, MdCheckboxModule} from "@angular/material";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MdButtonModule, //import all desired material-modules here!
+    MdCheckboxModule
   ],
   providers: [],
   bootstrap: [AppComponent]
