8344e40c5e171e8037a6fb3404c9dada08b242c9
Dan <DanDudemeister@users.noreply.github.com>
2018-01-26 18:30:12 +0100

    implemented interpolation-component

--- src/app/interpolation/interpolation.component.ts
+++ src/app/interpolation/interpolation.component.ts
@@ -1,15 +1,47 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {DomSanitizer, SafeHtml} from "@angular/platform-browser";
 
 @Component({
-  selector: 'app-interpolation',
-  templateUrl: './interpolation.component.html',
-  styleUrls: ['./interpolation.component.css']
+    selector: 'app-interpolation',
+    templateUrl: './interpolation.component.html',
+    styleUrls: ['./interpolation.component.css']
 })
 export class InterpolationComponent implements OnInit {
 
-  constructor() { }
+    public property: string;
+    public componentHtml: SafeHtml;
+    public templateHtml: SafeHtml;
 
-  ngOnInit() {
-  }
+    constructor(private sanitized: DomSanitizer) {
+        this.property = 'interpolation';
 
+        this.componentHtml = this.sanitized.bypassSecurityTrustHtml(`
+            import {Component, OnInit} from '@angular/core';
+            import {DomSanitizer} from "@angular/platform-browser";
+            
+            @Component({
+                selector: 'app-interpolation',
+                templateUrl: './interpolation.component.html',
+                styleUrls: ['./interpolation.component.css']
+            })
+            export class InterpolationComponent implements OnInit {
+            
+                public property: string;
+            
+                constructor() {
+                    this.property = 'interpolation';
+                }
+            
+                ngOnInit() {}
+            }
+        `);
+
+        this.templateHtml = this.sanitized.bypassSecurityTrustHtml(`
+            &lt;p&gt;
+                {{ property }} works!
+            &lt;/p&gt;
+        `);
+    }
+
+    ngOnInit() {}
 }
